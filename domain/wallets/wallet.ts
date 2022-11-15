e97a58259e6e7a7a480cdb9a18dd33096e3ee905
Jean <jean.carlosmolossi@gmail.com>
2022-11-14 22:58:52 -0300

    feat: added enums from wallet

--- domain/wallets/wallet.ts
+++ domain/wallets/wallet.ts
@@ -1,8 +1,21 @@
+export enum WalletType {
+	"CASH" = "CASH",
+	"CREDIT" = "CREDIT",
+	"DEBIT" = "DEBIT",
+}
+
+export enum WalletBrand {
+	'VISA' = 'VISA',
+	'MASTER' = 'MASTER',
+	'ELO' = 'ELO',
+	'AMERICAN_EXPRESS' = 'AMERICAN_EXPRESS',
+}
+
 export interface Wallet {
 	id: string;
 	name: string;
-	type: "CASH" | "CREDIT" | "DEBIT";
-	brand?: 'VISA' | 'MASTER' | 'ELO' | 'AMERICAN_EXPRESS';
+	type: WalletType;
+	brand?: WalletBrand;
 	limit: null | number,
 	readable_limit: null | string,
 	_self: string;
