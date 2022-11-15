595ac0333e0360309dc5eba547d8bcf21f4ec49c
Jean <jean.carlosmolossi@gmail.com>
2022-11-14 21:52:21 -0300

    feat: missing auth redirect

--- app/(auth)/error.tsx
+++ app/(auth)/error.tsx
@@ -1,7 +1,13 @@
 'use client';
 
+import { redirect } from "next/navigation";
+
 export default function Error({ error, reset }) {
 	console.log({error})
+
+	if (['Unauthorized', 'Missing authentication'].includes(error.message)) {
+		redirect('/login')
+	}
 
 	return (
 		<div>Oops</div>
