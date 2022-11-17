128feb28383181e432edf9ee0308b82d0a3c79aa
Jean <jean.carlosmolossi@gmail.com>
2022-11-16 23:25:06 -0300

    chore: added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html>
 		<head>
+			<link rel="shortcut icon" href='/favicon.ico' />
 			<meta charSet="UTF-8" />
 			<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
