7f2c5e2a8555b38fad1bc18e16b7dcf0a4529546
Jean <jean.carlosmolossi@gmail.com>
2022-11-26 13:58:55 -0300

    chore: using environments

--- src/constants.ts
+++ src/constants.ts
@@ -1,11 +1,6 @@
-const shouldOpenToNetwork = true;
-const networkHOST = shouldOpenToNetwork
-	? '192.168.100.11'
-	: 'localhost';
-
 const constants = {
-	API_BASE_URL: `http://${networkHOST}:3001`,
-	HOST_URL: `http://${networkHOST}:3000`
+	API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL ?? `http://localhost:3001`,
+	HOST_URL: process.env.NEXT_PUBLIC_HOST_URL ?? `http://localhost:3000`
 }
 
 export default constants;
